0fdbb618a6a1f19ef62a511d7cb77ecd85fa88eb
Martin <mr.mmaurer+work@gmail.com>
2019-07-23 21:16:48 +0200

    Added two way data binding for selectbox

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,43 @@
 	import Hero from './Hero.svelte';
 	import Header from './Header.svelte';
 
+	// props
+	// export let name;
+
 	// data
 	const dummyData = ['Mittens', 'Murle'];
+	const players = [
+		{
+			id: 4916,
+			fullName: 'Toby Alderweireld',
+		},
+		{
+			id: 4148,
+			fullName: 'Yaya Touré',
+		},
+		{
+			id: 2064,
+			fullName: 'Wayne Rooney',
+		},
+		{
+			id: 4246,
+			fullName: 'Per Mertesacker',
+		},
+		{
+			id: 8983,
+			fullName: 'Riyad Mahrez',
+		}
+	];
 
-	export let name;
+	function getPlayerNameById(id) {
+		const playerFound = players.find((player) => player.id === id);
+
+		return (playerFound === undefined) ? '' : playerFound.fullName;
+	}
+
+	// vars
+	let activePlayerId = 0;
+
 </script>
 
 <style type='text/scss'>
@@ -17,16 +50,23 @@
 	}
 
 	h1 {
-		color: red;
+		color: black;
 	}
 </style>
 
-<!-- layout -->
+<!-- markup -->
 <div class="wrapper">
 	<article class="page">
+		<div class="nav">
+			<select bind:value={ activePlayerId }>
+				<option value={ 0 } selected disabled>Select player...</option>
+				{#each players as player (player.id)}
+					<option value={player.id}>{player.fullName}</option>
+				{/each}
+			</select>
+		</div>
+		<h1>Selected player: { getPlayerNameById(activePlayerId) }</h1>
 		<Hero catName={dummyData[0]} />
 		<Header catName={dummyData[1]} />
-
-		<h1>Test4 {name}!</h1>
 	</article>
 </div>
